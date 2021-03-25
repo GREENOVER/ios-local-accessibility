bf0b53fb8bb5ac5cb7f4a979844374545d3de6b4
GREEN <humains29@gmail.com>
2021-03-25 15:56:51 +0900

    feat: 지역화 추가

--- LocalAccessbility/LocalAccessbility/ViewController.swift
+++ LocalAccessbility/LocalAccessbility/ViewController.swift
@@ -7,14 +7,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var currencyLabel: UILabel!
+    
+    let locale = Locale.current
+    let testLocale = Locale(identifier: "fr_FR")
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
     private func addBottomLabel() {
         // Do any additional setup after loading the view.
         
         let bottomLabel = UILabel()
         bottomLabel.frame = CGRect(x: 20, y: view.bounds.size.height - 150, width: view.bounds.size.width - 40, height: 50)
         
-        bottomLabel.text = "Touch the button below!"
+        bottomLabel.text = "Touch the button below!".localized
         
         bottomLabel.font = UIFont.systemFont(ofSize: 20)
         
@@ -30,7 +37,7 @@ class ViewController: UIViewController {
         let button = UIButton(type: .system)
         button.frame = CGRect(x: 20, y: view.bounds.size.height - 80, width: view.bounds.size.width - 40, height: 50)
         
-        button.setTitle("Touch Me!", for: .normal)
+        button.setTitle("Touch Me!".localized, for: .normal)
         
         button.titleLabel?.font = UIFont.systemFont(ofSize: 20)
         
@@ -44,8 +51,36 @@ class ViewController: UIViewController {
         addBottomLabel()
         addButton()
         
+        currency()
+        numbers()
+        date()
+        
+        view.semanticContentAttribute = .forceRightToLeft
     }
-
-
+    
+    func currency() {
+        let price = 3000.33 as NSNumber
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.currencyCode = locale.currencyCode
+        formatter.locale = locale
+        formatter.string(from: price)
+        currencyLabel.text = formatter.string(from: price)
+    }
+    
+    func numbers() {
+        let quantity = NumberFormatter.localizedString(from: 5000, number: .decimal)
+        numberLabel.text = String.localizedStringWithFormat(quantity)
+    }
+    
+    func date() {
+        let date = DateFormatter.localizedString(from: Date(), dateStyle: .medium, timeStyle: .short)
+        dateLabel.text = date
+    }
 }
 
+extension String {
+    var localized: String {
+        return NSLocalizedString(self, tableName: nil, bundle: Bundle.main, value: "", comment: "")
+    }
+}
